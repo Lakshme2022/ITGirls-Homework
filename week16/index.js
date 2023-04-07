e08ee3ed57d82a7f7e2bdf406bee97db06a26291
Katerina Sitnikov <lakshme@me.com>
2023-04-07 13:07:00 +0100

    no message

--- week16/index.js
+++ week16/index.js
@@ -4,36 +4,32 @@ const makers = form.elements.makers;
 const models = form.elements.models;
 const conditions = form.elements.condition;
 const ownersNumber = document.getElementById('ownersNumber');
-const fuel = form.elements.fuel;
-const owners = form.elements.owners;
-const year = form.elements.year;
-const payment = form.elements.payment;
 
 function CHOOSE(){
 
-    if(makers.value === "kia"){
-        models.innerHTML = `<option value="ceed">Ceed</option>
-       <option value="rio">Rio</option>
-       <option value="soul">Soul</option>
-       <option value="sportage">Sportage</option>`
+    if(makers.value === "Honda"){
+        models.innerHTML = `<option value="Accord">Accord</option>
+    <option value="Civic">Civic</option>
+    <option value="Cr-V">CR-V</option>
+    <option value="Odyssey">Odyssey</option>`
     }
-    if(makers.value === "honda"){
-        models.innerHTML = `<option value="accord">Accord</option>
-    <option value="civic">Civic</option>
-    <option value="crv">CR-V</option>
-    <option value="odyssey">Odyssey</option>`
+    if(makers.value === "Kia"){
+        models.innerHTML = `<option value="Ceed">Ceed</option>
+       <option value="Rio">Rio</option>
+       <option value="Soul">Soul</option>
+       <option value="Sportage">Sportage</option>`
     }
-    if(makers.value === "renault") {
-        models.innerHTML = `<option value="clio">Clio</option>
-    <option value="duster">Duster</option>
-    <option value="logan">Logan</option>
-    <option value="megan">Megan</option>`
+    if(makers.value === "Renault") {
+        models.innerHTML = `<option value="Clio">Clio</option>
+    <option value="Duster">Duster</option>
+    <option value="Logan">Logan</option>
+    <option value="Megan">Megan</option>`
     }
-    if(makers.value === "volkswagen") {
-        models.innerHTML = `<option value="golf">Golf</option>
-    <option value="polo">Polo</option>
-    <option value="tiguan">Tiguan</option>
-    <option value="touareg">Touareg</option>`
+    if(makers.value === "Volkswagen") {
+        models.innerHTML = `<option value="Golf">Golf</option>
+    <option value="Polo">Polo</option>
+    <option value="Tiguan">Tiguan</option>
+    <option value="Touareg">Touareg</option>`
     };
 }
 
@@ -41,12 +37,16 @@ CHOOSE();
 
 makers.addEventListener("change", CHOOSE);
 
+let isNew = 0;
+
 function OWNERS(e){
 
     if(e.target.value === "new"){
+        isNew = 0;
         ownersNumber.innerHTML = "";
     }
     if(e.target.value === "old"){
+        isNew = 1;
         ownersNumber.innerHTML = `<p>Владельцев по ПТС</p>
 
     <input type="radio" name="owners" id="1" value="1">
@@ -64,23 +64,76 @@ for(let cond of conditions){
     cond.addEventListener("change", OWNERS);
 }
 
+const makersArray = ['Honda', 'Kia', 'Renault', 'Volkswagen'];
+const makersCostArray = [2000000, 1200000, 1000000, 1500000];
+const modelsArray = [
+    ['Accord', 'Civic', 'Cr-V', 'Odyssey'],
+    ['Ceed', 'Rio', 'Soul', 'Sportage'],
+    ['Clio', 'Duster', 'Logan', 'Megan'],
+    ['Golf', 'Polo', 'Tiguan', 'Touareg']
+];
+const modelCostModifier = [
+    [1.35, 1.4, 1.75, 3],
+    [1.17, 1.8, 2, 3.1],
+    [1.5, 2, 1.2, 1.6],
+    [1.8, 1.13, 1.65, 5.3]
+];
+const butt = document.getElementById('butt');
+const result = document.getElementById('result');
+const fuel = form.elements.fuel;
+const year = form.elements.year;
+const payment = form.elements.payment;
 
+const fuelMod = [
+    [0, .07, .05, .03],
+    [0, -.02, .03, .01]
+];
+const fuelArray = ['petrol', 'electrical', 'hybrid', 'diesel',];
+const yearArray = ['2023-2020', '2019-2015', '2014-2008', '2007'];
+const ownersArray =['1', '2', '3', '4'];
+const ownersMod = [.06, .04, .02, 0];
+const yearMod = [-.25, -.45, -.60, -.75];
 
+function PRICE() {
 
+    let makerIndex = makersArray.indexOf(makers.value);
+    let modelIndex = modelsArray[makerIndex].indexOf(models.value);
+    let modelPrice = makersCostArray[makerIndex] * modelCostModifier[makerIndex][modelIndex];
 
+    let fuelIndex = fuelArray.indexOf(fuel.value);
 
+    let Mod = fuelMod[isNew][fuelIndex];
 
+    if (isNew === 1) {
+        const owners = form.elements.owners;
+        let ownersIndex = ownersArray.indexOf(owners.value);
+        let yearIndex = yearArray.indexOf(year.value);
+        Mod += yearMod[yearIndex];
+        Mod += ownersMod[ownersIndex];
+    }
+    if (payment.value === "cash") {
+        Mod -= .05;
+    }
+    if (payment.value === "entity") {
+        Mod += .05;
+    }
 
+    let finalPrice = modelPrice + modelPrice * Mod;
 
+    let addResult = `<h2>Оценочная стоимость:</h2>
+        <h2 class="result_price">${finalPrice}</h2>
+        <h4>Марка:</h4>
+        <h4 class="result_makes">${makers.value}</h4>
+        <h4>Модель:</h4>
+        <h4 class="result_model">${models.value}</h4>
+        <h4>Год производства:</h4>
+        <h4 class="result_year">${year.value}</h4>
+        <h4>Тип топлива:</h4>
+        <h4 class="result_fuel">${fuel.value}</h4>`;
 
-//
-// <h1>Оценочная стоимость:</h1>
-// <h1 class="result_price"></h1>
-// <h3>Марка:</h3>
-// <div class="result_makes"></div>
-// <h3>Модель:</h3>
-// <div class="result_model"></div>
-// <h3>Тип топлива:</h3>
-// <div class="result_fuel"></div>
-// <h3>Год производства:</h3>
-// <div class="result_year"></div>
+    result.innerHTML = addResult;
+
+};
+
+butt.addEventListener('click', PRICE);
+
