8044075b52c0d017b143a0be9e03cc996d9fac47
Katerina Sitnikov <lakshme@me.com>
2023-10-26 23:39:25 +0100

    no message

---
+++ react24/src/App.js
@@ -0,0 +1,47 @@
+import './style/App.scss';
+import Tariff from "./components/Tariff/Tariff";
+import {useState} from "react";
+
+
+const array = [
+  {
+    price: 300,
+    speed: 10,
+    color: 'blue'
+  },
+  {
+    price: 450,
+    speed: 50,
+    color: 'green'
+  },
+  {
+    price: 550,
+    speed: 100,
+    color: 'red',
+  },
+  {
+    price: 1000,
+    speed: 200,
+    color: 'black'
+  }
+]
+
+
+
+export default function App() {
+  const [id, setId] =  useState(0);
+
+  return (
+    <div className="App">
+      {array.map((item, index) =>
+          <Tariff
+              key={index}
+              id={index+1}
+              {...item}
+              item={item}
+              selected={index+1 === id}
+              setId={setId}/>)}
+    </div>
+  );
+}
+
---
+++ react24/src/components/Tariff/Tariff.jsx
@@ -0,0 +1,25 @@
+import './Tariff.scss';
+
+export default function Tariff({id, item, price, speed, color, selected, setId}) {
+
+    function handleSelected (){
+        setId(id)
+    }
+
+    return (
+        <div className='card' onClick={handleSelected}>
+        <div className={selected ? "container selected" : "container"}>
+                <div className={color}>
+                <div className='header grid'>Безлимитный {price}</div>
+                    <div className='table'>
+                         <div className='text rub'>руб</div>
+                         <div className='number grid'>{price}</div>
+                         <div className='month'>/мес</div>
+                    </div>
+                </div>
+                <div className='text speed grid'>до {speed} Мбит/сек</div>
+                <div className='text footer grid'>Объем включенного трафика не ограничен</div>
+        </div>
+        </div>
+    );
+};
