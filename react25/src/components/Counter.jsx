fa495019f372350f1d75c74207b2ffd47ee19039
Katerina Sitnikov <lakshme@me.com>
2023-10-29 14:57:30 +0000

    no message

--- react25/src/components/Counter.jsx
+++ react25/src/components/Counter.jsx
@@ -2,9 +2,9 @@ import './Counter.css';
 import React, {useState} from 'react';
 
 function Counter () {
-    const [count, setState] = useState(0);
+    const [count, setCount] = useState(0);
     const handleClick = () => {
-        setState(count + 1);
+        setCount(count + 1);
     };
     return(
         <button onClick={handleClick}>{count}</button>
